70d0488e8290ae868c62834283b0063070c66088
divojaparamajyothikakurella-lab <divojaparamajyothikakurella@gmail.com>
2026-03-30 10:18:20 +0530

    Refactor main method into Test class

--- test.java
+++ test.java
@@ -1,4 +1,6 @@
 import java.util.*;
-public static void main(String args[]){
-  System.out.println("Hello world");
-} 
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
